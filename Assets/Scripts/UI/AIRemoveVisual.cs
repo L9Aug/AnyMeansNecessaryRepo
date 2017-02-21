7084169dfc92631b1826058e93cfc62452013612
Arby27 <arby27@live.co.uk>
2017-02-21 12:56:05 +0000

    AI visulised

---
+++ Assets/Scripts/UI/AIRemoveVisual.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIRemoveVisual : MonoBehaviour {
+
+
+    public GameObject AI;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(AI.GetComponent<Standard_Enemy>()._state == Base_Enemy.State.Dead)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
